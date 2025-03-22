3aeade81eb6c0f700decb09e27dd5179a3d00a93
Aloktiwari-18 <151405119+Aloktiwari-18@users.noreply.github.com>
2025-03-22 20:08:50 +0000

    Datatype Summar

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let str1="hello"
+let str2="hii"
+console.log(str1+str2)
+
+console.log("1"+2)//=>12
+
+console.log(1+"2")//=> 12
+
+console.log("1"+2+2)//=>122
+
+
+colsole.log(1+2+"2")//=>32
+
+console.log(+true)//=>1
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive 
+
+//7 primitive types:String,Number , Boolean,null,undefined ,Symbols,BigInt
+
+
+const name="Alok"
+const age=19
+
+const namae=true;
+const outsideTemp=false
+const id=Symbol('123')
+const anotherId=Symbol("123")
+console.log(id===anotherId)
+
+const bigNum=2323435435324
+
+
+// reference (Non primitive)
+// Array ,Object,Functions
+
+
+const heros=["Alok","Aman","Abhi","Akhil"]
+
+const myObj={
+    name:"alok",
+    age:12,
+
+}
+
+const myFunction=function(){
+    console.log("Hello World")
+}
